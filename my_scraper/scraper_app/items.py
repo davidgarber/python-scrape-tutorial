edb244382061462bd1b89eeb92f4503c175da1b2
David Garber <davidgarber@gmail.com>
2015-09-18 18:17:17 -0700

    Create items.py page.

--- my_scraper/scraper_app/items.py
+++ my_scraper/scraper_app/items.py
@@ -1,4 +1,12 @@
 from scrapy.item import Item, Field
 
 class LivingSocialDeal(Item):
-    """Livingsocial container (dictionary-like object) for scraped data""" 
+    """Livingsocial container (dictionary-like object) for scraped data"""
+    title = Field()
+    link = Field()
+    location = Field()
+    original_price = Field()
+    price = Field()
+    end_Date = Field()
+
+    
